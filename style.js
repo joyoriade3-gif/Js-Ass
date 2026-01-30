c02f8f9704a70930470e86e76262b47d4f134a83
JoyOriade <joyoriade3@gmail.com>
2026-01-30 06:21:05 -0800

    first commit

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
-// let email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/;
-// console.log(email.test("joyoriAde3@gmail.com"));
+let email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/;
+console.log(email.test("joyoriAde3@gmail.com"));
 
 const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{1,}$/;
 console.log(emailPattern.test("joyoriAde3@gmail.uk.ng")); // true
